5fd638688b17a108a1b5a1c0f208185ae22415cc
Yannick <lanfyp@gmail.com>
2023-02-23 23:16:01 +0100

    Ajout bootstrap profiler validator et annotations + ajout contraintes dans post

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+
+
 class Post
 {
     private int $id;
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -9,16 +9,43 @@ use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Validator\Constraints as Assert;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add("title", TextType::class, ["label" => "Titre", "required" => false])
-            ->add("content", TextareaType::class, ["label" => "Contenu", "required" => true])
-            ->add("image", UrlType::class, ["label" => "URL de l'image", "required" => false]);
+            ->add("title", TextType::class, [
+                "label" => "Titre",
+                "required" => false,
+                "constraints" => [
+                    new Assert\Length([
+                        "min" => 0,
+                        "max" => 150,
+                        "minMessage" => "Le titre ne doit pas dépasser 150 caractères",
+                        "maxMessage" => "Le titre ne doit pas dépasser 150 caractères"
+                    ])]
+                ])
+            ->add("content", TextareaType::class, [
+                "label" => "Contenu", 
+                "required" => true,
+                "constraints" => [
+                    new Assert\NotBlank(["message" => "Le contenue est obligatoire"]),
+                    new Assert\Length([
+                        "min" => 5,
+                        "max" => 350,
+                        "minMessage" => "Le contenue doit faire entre 5 et 350 caractères",
+                        "maxMessage" => "Le contenue doit faire entre 5 et 350 caractères"
+                    ])]
+                    ])
+            ->add("image", UrlType::class, [
+                "label" => "URL de l'image",
+                "required" => false,
+                "constraints" => [
+                    new Assert\Url(["message" => "L'URL doit être celle d'une image valide"])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
