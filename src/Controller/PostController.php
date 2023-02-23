db85eddf3403ab928a0e381aa3216162c39e5c35
Yannick <lanfyp@gmail.com>
2023-02-23 12:05:32 +0100

    Mise en place formulaire new post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controller;
-
+ 
+use App\Form\PostType;
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +15,17 @@ class PostController extends AbstractController
     {
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
+        ]);
+    }
+
+    #[Route('/post/new')]
+    public function create(): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+
+        return $this->render('post/form.html.twig', [
+            'post_form' => $form->createView(),
         ]);
     }
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -23,7 +23,7 @@ class PostType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Post::class,
+            'data_class' => Post::class
         ]);
     }
 }
